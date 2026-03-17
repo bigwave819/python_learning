c45b7f1f5b5c0e11cbf6b950bee7b9cc552ffe38
Hirwa Tresor <waveb6133@gmail.com>
2026-03-17 20:24:52 +0200

    created the crud api with fast api on my Day 2 of learning the python for web development used mongoDb

---
+++ api/database.py
@@ -0,0 +1,8 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+
+MongoDbUrl = "mongodb://127.0.0.1:27017/expense"
+
+client = AsyncIOMotorClient(MongoDbUrl)
+database = client["fast_crud"]
+
+user_collection = database["none"]
